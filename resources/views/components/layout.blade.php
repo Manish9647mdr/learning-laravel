8701ae509972c159d7d3d1c7efc87ec00f024009
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2025-10-20 18:29:56 +0900

    tailwind CSS を使用してlayoutの追加

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,16 +6,63 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home Page</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
-    <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+    <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
+    <nav class="relative bg-gray-800">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div class="relative flex h-16 items-center justify-between">
+
+                <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                    {{-- <div class="flex shrink-0 items-center">
+                        <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                            alt="Your Company" class="h-8 w-auto" />
+                    </div> --}}
+                    <div class="hidden sm:ml-6 sm:block">
+                        <div class="flex space-x-4">
+                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+                            <a href="/" aria-current="page"
+                                class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">Home</a>
+                            <a href="/about"
+                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                            <a href="/contact"
+                                class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <el-disclosure id="mobile-menu" hidden class="block sm:hidden">
+            <div class="space-y-1 px-2 pt-2 pb-3">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+                <a href="/" aria-current="page"
+                    class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white">Home</a>
+                <a href="/about"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                <a href="/contact"
+                    class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+            </div>
+        </el-disclosure>
     </nav>
 
-    {{ $slot }}
+    <header class="bg-white border">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:bx-8">
+            <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+                {{ $heading }}
+            </h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            {{ $slot }}
+        </div>
+    </main>
 </body>
 
 </html>
