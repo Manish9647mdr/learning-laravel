36ae3392900af6e47aceae70f439e8c6fab00f1f
Manandhar Manish <maanish98mdr@gmail.com>
2025-10-21 15:09:33 +0900

    Add Employer model, factory, and migration; update Job model and factories

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -1,14 +1,11 @@
 <?php
 
 namespace App\Models;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
 
-class Job extends Model
+class Employer extends Model
 {
     use HasFactory;
-    protected $table = 'jobs_listings';
-
-    protected $fillable = ['title', 'salary'];
 }
