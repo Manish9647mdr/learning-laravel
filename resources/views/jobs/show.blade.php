006ed7036652c89ede4bf500be47ab616b2995f6
Manandhar Manish <maanish98mdr@gmail.com>
2025-11-10 17:54:49 +0900

    Jobの修正、更新と削除サービスの追加

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -3,8 +3,12 @@
         Job
     </x-slot:heading>
 
-    <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
+    <h2 class="font-bold text-lg">{{ $job->title }}</h2>
     <p>
-        This job pay {{ $job['salary'] }} per year.
+        This job pay {{ $job->salary }} per year.
+    </p>
+
+    <p class="mt-6">
+        <x-forms.button href="/jobs/{{ $job->id }}/edit">Edit Job</x-forms.button>
     </p>
 </x-layouts.layout>
--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ Route::get('/', function () {
     return view('home');
 });
 
+// index
 Route::get('/jobs', function () {
     $jobs = Job::with('employer')->simplePaginate(4);
     return view('jobs.index', [
@@ -14,15 +15,18 @@ Route::get('/jobs', function () {
     ]);
 });
 
+// create
 Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
+// show
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
     return view('jobs.show', ['job' => $job]);
 });
 
+// store
 Route::post('/jobs', function () {
     // validation
     request()->validate([
@@ -36,6 +40,35 @@ Route::post('/jobs', function () {
         'employer_id'=>1
     ]);
 
+    return redirect('/jobs');
+});
+
+// edit
+Route::get('/jobs/{id}/edit', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.edit', ['job' => $job]);
+});
+
+// update
+Route::patch('/jobs/{id}', function ($id) {
+    request()->validate([
+        'title' =>['required', 'min:5'],
+        'salary'=>['required']
+    ]);
+    // authorize
+    $job =Job::find($id);
+
+    $job->update([
+        'title'=>request('title'),
+        'salary'=>request('salary'),
+    ]);
+
+    return redirect('/jobs/'.$job->id);
+});
+
+// delete
+Route::delete('/jobs/{id}', function ($id) {
+     Job::findOrFail($id)->delete();
     return redirect('/jobs');
 });
 
